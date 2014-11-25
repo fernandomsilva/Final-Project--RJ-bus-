f0ee069f293f785827a2e82dc517ac03fcd57aa4
Aline Bessa <alibezz@172-16-16-79.dynapool.nyu.edu>
2014-11-24 20:55:22 -0500

    Adding more scripts that are currently running on Amazon

---
+++ Map-Reduce/StopsCount.java
@@ -0,0 +1,68 @@
+import java.io.IOException;
+import java.util.*;
+import java.io.*;        
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+        
+public class StopsCount {
+        
+ public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+    List<List<String>> gtfs_tuples; 
+
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+      /* GTFS fields: linha,descricao,agencia,sequencia,latitude,longitude */
+      String line = value.toString();
+      List<String> fields = Arrays.asList(line.split(","));
+      String busLine = fields.get(0);
+      word.set(busLine);
+      context.write(word, one);
+    }
+ }
+ 
+       
+ public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) 
+      throws IOException, InterruptedException {
+ 
+      int sum = 0;
+      for(IntWritable val : values) {
+         sum += val.get();
+      }
+
+      context.write(key, new IntWritable(sum));
+    }
+ }
+    
+ public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+        
+    Job job = new Job(conf, "stopscount");
+    
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+        
+    job.setMapperClass(Map.class);
+    job.setReducerClass(Reduce.class);
+        
+    job.setInputFormatClass(TextInputFormat.class);
+    job.setOutputFormatClass(TextOutputFormat.class);
+
+    job.setNumReduceTasks(1);
+    job.setJarByClass(StopsCount.class);
+        
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        
+    job.waitForCompletion(true);
+ }
+        
+}
